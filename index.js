5cfb49c44635251507346da66ed45b63c38d09d3
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-01-27 17:55:18 -0700

    commit tree b1e837998331b22a4691e600ac92dc3e7fbec8dc

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+
+class Polygon {
+    constructor(arrayOfIntegers) {
+        this.arrayOfIntegers = arrayOfIntegers
+    }
+
+    get countSides() {
+        return this.arrayOfIntegers.length
+    }
+
+    get perimeter() {
+        const totalSides = this.arrayOfIntegers.reduce((acc, i) => {
+            return i + acc;
+        }, 0);
+        return totalSides;
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let side1 = this.arrayOfIntegers[0];
+        let side2 = this.arrayOfIntegers[1];
+        let side3 = this.arrayOfIntegers[2];
+        if (side1 + side2 > side3 && side2 + side3 > side1 && side3 + side1 > side2) {
+            return true;
+        } else {
+            return false
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        let side1 = this.arrayOfIntegers[0];
+        let side2 = this.arrayOfIntegers[1];
+        let side3 = this.arrayOfIntegers[2];
+        let side4 = this.arrayOfIntegers[3];
+        if (side1 === side2 && side2 === side3 && side3 === side4) {
+            return true 
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        if (this.isValid === true) {
+            let side1 = this.arrayOfIntegers[0];
+            let side2 = this.arrayOfIntegers[1];
+            return side1 * side2;
+        } else {
+            return "This is not a valid square"
+        }
+    }
+}
